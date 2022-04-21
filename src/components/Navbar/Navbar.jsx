e1f0790c896b5eebff3261eb09ab7531ee9d35ab
borismg04 <borismg04@gmail.com>
2022-04-20 21:55:55 -0500

    estilo navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './Navbar.module.css';
+
+function Navbar() {
+  return (
+    <div className={styles.Navbar__container}>
+      <NavLink className={styles.letter_navbar} to="/">Home</NavLink>
+      <NavLink className={styles.letter_navbar} to="/about">Products Details</NavLink>
+      <NavLink className={styles.letter_navbar} to="/about">About</NavLink>
+    </div>
+  );
+}
+
+export default Navbar;
